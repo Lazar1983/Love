826804e211c4a22402f255305fadf8306e291d94
Lazar1983 <lazov_laza@yahoo.com>
2020-06-03 00:15:23 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './love.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/lazarlazov83/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LEPA I LOVE YOU
         </a>
       </header>
     </div>
