da6934f8e65ab01781d31f7db25180bba2b371f4
Lazar1983 <lazov_laza@yahoo.com>
2020-06-03 00:14:51 +0200

    Updates

---
+++ precache-manifest.8a9224810ddea8287fb42ef9a88ad630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b89652f835cd0143f11f7fa4b75956",
+    "url": "/Love/index.html"
+  },
+  {
+    "revision": "737abd482b70d0c15c50",
+    "url": "/Love/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5294d4feade9823fb9a",
+    "url": "/Love/static/js/2.e2936145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Love/static/js/2.e2936145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737abd482b70d0c15c50",
+    "url": "/Love/static/js/main.b5e1f901.chunk.js"
+  },
+  {
+    "revision": "95ac83bec00cba79a543",
+    "url": "/Love/static/js/runtime-main.c984a01c.js"
+  },
+  {
+    "revision": "deb2bf8da2f75e90983359c9a8a2723a",
+    "url": "/Love/static/media/love.deb2bf8d.jpg"
+  }
+]);
